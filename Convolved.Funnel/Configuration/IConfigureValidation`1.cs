1f5f71b4399f386aad56727d04747f59e3396bc8
Aaronaught <convolved@outlook.com>
2013-03-30 16:59:00 -0400

    Better extensibility in IConfigureValidation.

--- Convolved.Funnel/Configuration/IConfigureValidation`1.cs
+++ Convolved.Funnel/Configuration/IConfigureValidation`1.cs
@@ -5,6 +5,8 @@ namespace Convolved.Funnel.Configuration
 {
     public interface IConfigureValidation<T> : IConfigureErrorHandling<T>
     {
-        IConfigureValidation<T> ValidateWith<TValidator>() where TValidator : IValidate<T>;
+        IConfigureValidation<T> ValidateWith(IValidate<T> validator);
+        IConfigureValidation<T> ValidateWith<TValidator>(params object[] parameters)
+            where TValidator : IValidate<T>;
     }
 }
