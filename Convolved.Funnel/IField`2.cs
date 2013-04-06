d94ced56461daeec9575462abed628ddbe4cd503
Aaronaught <convolved@outlook.com>
2013-04-06 12:23:07 -0400

    Added generic field interfaces and made ITextField derive from it.

---
+++ Convolved.Funnel/IField`2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Convolved.Funnel
+{
+    /// <summary>
+    /// Encapsulates a field for a particular type of file.
+    /// </summary>
+    /// <typeparam name="TContext">The type of context which indicates the type of file being
+    /// read.</typeparam>
+    /// <typeparam name="TData">The type of data that is read from the file. This is usually either
+    /// text (<see cref="System.String"/>) or binary (<see cref="System.Byte[]"/>).</typeparam>
+    public interface IField<TContext, TData>
+        where TContext : FileContext
+    {
+        /// <summary>
+        /// Reads data from the specified file.
+        /// </summary>
+        /// <param name="context">The context representing the file to be read..</param>
+        /// <returns>A <see cref="TData"/> instance read from the current file position using this
+        /// field definition.</returns>
+        TData ReadValue(TContext context);
+    }
+}
--- Convolved.Funnel/Text/ITextField.cs
+++ Convolved.Funnel/Text/ITextField.cs
@@ -5,13 +5,13 @@ namespace Convolved.Funnel.Text
     /// <summary>
     /// Encapsulates a field in a text file or section.
     /// </summary>
-    public interface ITextField
+    public interface ITextField : IField<TextFileContext, string>
     {
         /// <summary>
         /// Reads the current field value.
         /// </summary>
         /// <param name="context">The context indicating the current line and position within a file.</param>
         /// <returns>The field value within the specified <paramref name="context"/>.</returns>
-        string ReadValue(TextFileContext context);
+        new string ReadValue(TextFileContext context);
     }
 }
